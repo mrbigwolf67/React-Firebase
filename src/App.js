74809fba12aaf26189da6ddb09ecac0903f6a1b1
mrbigwolf67 <uffec@comhem.se>
2019-10-31 11:42:13 +0100

    Added React module replacements

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React nu
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App />, rootEl );
+}
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        setTimeout(render);
+    })
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// Just trying to push origin master branch
 serviceWorker.unregister();
